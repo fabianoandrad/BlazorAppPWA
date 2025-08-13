2a782df9403e363ed7868077989d5710c9ff592c
Fabiano Andrade <affatecnologia@outlook.com.br>
2025-08-12 21:01:15 -0300

    Add project files.

---
+++ Pages/Home.razor.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace BlazorAppPWA.Pages
+{
+    public partial class Home : ComponentBase
+    {
+        [Inject]
+        IJSRuntime? JS { get; set; }
+
+        public bool visibleDialogInstallApp { get; set; } = true;
+
+
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                var isInstalled = await JS.InvokeAsync<bool>("pwaHelper.isAppInstalled");
+                if (!isInstalled)
+                {
+                    Console.WriteLine("App não instalado");
+                    _ = Task.Run(() => JS.InvokeVoidAsync("pwaHelper.listenForInstallPrompt", DotNetObjectReference.Create(this)));
+                }
+            }
+        }
+
+        protected async override void OnInitialized()
+        {
+
+        }
+
+        [JSInvokable("ShowInstallPrompt")]
+        public void ShowInstallPrompt()
+        {
+            visibleDialogInstallApp = false;
+            StateHasChanged();
+        }
+
+        private async Task InstallApp()
+        {
+            await JS.InvokeVoidAsync("pwaHelper.triggerInstallPrompt");
+            visibleDialogInstallApp = true;
+        }
+
+    }
+}
---
+++ wwwroot/Utils/Notifications/notifyAppInstallation.js
@@ -0,0 +1,34 @@
+﻿window.pwaHelper = {
+  isAppInstalled: function () {
+    return window.matchMedia('(display-mode: standalone)').matches || window.navigator.standalone === true;
+  },
+    listenForInstallPrompt: function (dotNetHelper) {
+        try {
+            console.log('Entramos em Listening beforeinstallprompt event');
+
+            window.addEventListener("beforeinstallprompt", (event) => {
+                console.log('Entramos em beforeinstallprompt event');
+                event.preventDefault();
+                window.PWADeferredPrompt = event;
+                dotNetHelper.invokeMethodAsync('ShowInstallPrompt');
+            });
+
+            window.addEventListener("appinstalled", (event) => {
+                console.log('App instalado com sucesso!!!!!!!');
+            })
+        } catch (error) {
+            console.error('Error in listenForInstallPrompt:', error);
+        }
+  },
+    triggerInstallPrompt: function () {
+        console.log('Entramos em triggerInstallPrompt');
+
+        if (window.PWADeferredPrompt) {
+            window.PWADeferredPrompt.prompt();
+            window.PWADeferredPrompt.userChoice.then(function (choiceResult) {
+                window.PWADeferredPrompt = null;
+            });
+        }
+
+    }
+};
